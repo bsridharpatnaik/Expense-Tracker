b47fa4a4a2e60ca76e8fe2b12411085a5387d474
Sridhar Patnaik <bsridharpatnaik@gmail.com>
2024-08-25 18:28:05 +0530

    More changes

--- src/main/java/com/gb/et/security/services/UserDetailsImpl.java
+++ src/main/java/com/gb/et/security/services/UserDetailsImpl.java
@@ -1,6 +1,7 @@
 package com.gb.et.security.services;
 
 import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 
@@ -38,7 +39,7 @@ public class UserDetailsImpl implements UserDetails, Serializable {
 
   @Override
   public Collection<? extends GrantedAuthority> getAuthorities() {
-    return null;
+    return Collections.emptyList();
   }
 
   @Override
