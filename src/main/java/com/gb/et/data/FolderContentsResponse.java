05202da600da205406fdc0be490560933d2d79dd
Sridhar Patnaik <bsridharpatnaik@gmail.com>
2024-08-29 22:20:23 +0530

    Update FolderContentsResponse.java

--- src/main/java/com/gb/et/data/FolderContentsResponse.java
+++ src/main/java/com/gb/et/data/FolderContentsResponse.java
@@ -34,6 +34,15 @@ public class FolderContentsResponse {
 
     // Getters and Setters
 
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
     public List<FolderPathDto> getBreadcrumb() {
         return breadcrumb;
     }
